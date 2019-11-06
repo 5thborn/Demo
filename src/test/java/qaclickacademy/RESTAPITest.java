a69ad769373e46d3dcc68979c88be0a729252d2b
Kashan Curry <kashan5@verizon.net>
2019-11-06 15:15:12 -0800

    dev 2 update commited

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,6 +10,13 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira2");
+		
+		//NEXT DEV COMES IN TO START WHERE LAST DEV LEFT OFF
+		//NEW CODE BELOW
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+	
 	}
 	
 	@Test
